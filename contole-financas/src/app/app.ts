7d484e07b214560980bb1340a2fc4a374bb41e79
WilsonMANeto <162630712+WilsonMANeto@users.noreply.github.com>
2025-11-03 18:58:29 +0000

    feat: Criação Projeto Finanças

--- contole-financas/src/app/app.ts
+++ contole-financas/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('controle-financas');
+  protected readonly title = signal('contole-financas');
 }
--- controle-financas/src/app/transacao.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Transacao {
-  id: number;
-  descricao: string;
-  valor: number;
-  concluida: boolean;
-}
